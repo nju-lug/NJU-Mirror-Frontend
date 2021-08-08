d058600ff18ba219575e44cfd7a61f4cf19a4ed2
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-08-09 03:50:40 +0800

    finish search MirrorList

--- src/components/MirrorList.vue
+++ src/components/MirrorList.vue
@@ -1,32 +1,38 @@
 <template>
-  <el-table class="mirror-container"
-            :data="entries"
-            border
-            style="width: 100%">
-    <el-table-column prop="name"
-                     label="Mirror Name"
-                     width="220"
-    >
-    </el-table-column>
-    <el-table-column prop="status"
-                     label="Sync Status"
-                     width="150">
-    </el-table-column>
-    <el-table-column prop="last_update"
-                     label="Last Update"
-                     width="250">
-    </el-table-column>
-    <el-table-column prop="size"
-                     label="Mirror Size">
-    </el-table-column>
-    <el-table-column fixed="right"
-                     label="Operations"
-                     width="100">
-      <template slot-scope="scope">
-        <el-button @click="handleClick(scope.row)" type="text">View</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-input placeholder="Search mirror"
+              v-model="filter"
+              @input="filterList">
+      <i slot="prefix" class="el-input__icon el-icon-search"></i>
+    </el-input>
+    <el-table class="mirror-container"
+              :data="show"
+              border
+              style="width: 100%; margin-top: 10px">
+      <el-table-column prop="name"
+                       label="Mirror Name"
+                       width="220">
+      </el-table-column>
+      <el-table-column prop="status"
+                       label="Sync Status"
+                       width="150">
+      </el-table-column>
+      <el-table-column prop="last_update"
+                       label="Last Update"
+                       width="250">
+      </el-table-column>
+      <el-table-column prop="size"
+                       label="Mirror Size">
+      </el-table-column>
+      <el-table-column fixed="right"
+                       label="Operations"
+                       width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text">View</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script lang="ts">
@@ -35,14 +41,25 @@ import {SyncEntry, fetchEntries} from '@/models';
 
 export default Vue.extend({
   name: 'MirrorList',
+
   data() {
     return {
       entries: Array<SyncEntry>(),
+      filter: '',
     };
+  },
+  computed: {
+    show(): Array<SyncEntry> {
+      console.log(this);
+      return this.entries.filter(value => value.name.toLowerCase().includes(this.filter));
+    }
   },
   methods: {
     handleClick(item: SyncEntry) {
       this.$router.replace(item.path);
+    },
+    filterList(keyword: string) {
+      this.filter = keyword.toLowerCase();
     }
   },
   beforeMount() {
