abaf93f9e100158d69a487b63fbc3c36f730005f
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-08-11 00:35:56 +0800

    added LUG Jokes to Aside

--- src/configs/cards.ts
+++ src/configs/cards.ts
@@ -19,7 +19,21 @@ const friends: Promise<CardItem> = axios.get('/cards/friends.json').then(
   res => new Promise(resolve => resolve(res.data)),
 );
 
+const jokes = axios.get('https://git.nju.edu.cn/api/v4/projects/2412/issues').then(
+  res => new Promise(resolve => {
+    const links = (res.data as Array<{ title: string, web_url: string }>).map(
+      value => <LinkItem>{
+        title: value.title,
+        url: value.web_url,
+      });
+    resolve({title: 'LUG Jokes', links: links.slice(0, 5)});
+  }),
+  err => new Promise((resolve, reject) => reject(err)),
+);
+
+
 export default <Array<Promise<CardItem>>>[
+  jokes,
   esci,
   friends,
 ];
