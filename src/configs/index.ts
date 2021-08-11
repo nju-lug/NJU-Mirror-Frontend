ad4fb1efdaaeaa15e6ec770813e9537283be1494
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-08-11 22:03:50 +0800

    move server configs to /configs/

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,5 +1,7 @@
 import docConfig, {DocItem} from './docs';
 import linkConfig, {CardItem} from './cards';
+import {SyncEntry, fetchEntries} from './mirrors';
 import {serverPrefix} from './common';
 
-export {serverPrefix, DocItem, docConfig, linkConfig, CardItem};
+
+export {serverPrefix, DocItem, docConfig, linkConfig, CardItem, SyncEntry, fetchEntries};
