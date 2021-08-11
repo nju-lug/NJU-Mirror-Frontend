e5e232197321d4cd9347f13c9ac67877550f064d
Rollingegg <1244797125@qq.com>
2021-08-11 10:40:04 +0800

    Change link <button> to <a>; Change the collapse icon to 'float right'

--- src/components/TopNavi.vue
+++ src/components/TopNavi.vue
@@ -20,9 +20,9 @@
     </template>
 
     <el-submenu v-else
-                index="colapse">
+                index="colapse" style="float:right">
       <template slot="title">
-        <i class="el-icon-s-fold" style="font-size:28px;"/>
+        <i class="el-icon-s-fold" style="font-size:28px;color: #63065f"/>
       </template>
       <el-menu-item v-for="(item,key) in routes" :key="key"
                     :index="item.route">{{ item.name }}
