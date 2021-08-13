31676909ee865a67504d35349fc2c92e823077e5
Rollingegg <1244797125@qq.com>
2021-08-13 14:19:59 +0800

    Remove redundant ga script loading in index.html

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,7 @@ import VueAnalytics from 'vue-analytics';
 if (process.env.VUE_APP_GA) {
   Vue.use(VueAnalytics, {
     id: process.env.VUE_APP_GA, // 从配置中读取
-    disableScriptLoader: true, // 必须在html中完成初始化，这里显式禁止去下载ga脚本
     router, // 确保路由切换时可以自动统计
-    autoTracking: {
-      pageviewOnLoad: false // 当通过网址进来时已经GA在初始化时就发起一次pageview的统计，这里不要重复统计
-    }
   });
 }
 Vue.use(ElementUI);
