d9060a18a0668b80c894b25cb48ae819d31a25ae
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-08-21 07:22:35 +0800

    rewrite genjson.py

--- public/configs/documentations/genjson.py
+++ public/configs/documentations/genjson.py
@@ -1,22 +1,14 @@
+from typing import List, Dict
 import os
+import json
 
-docs = {}
-for file in os.listdir('.'):
-    if file[-3:] == '.md':
-        path = file
-        name = file[:-3].split('-')[3:]
-        name = '-'.join(name)
-        docs[name] = file
-names = list(docs.keys())
-names.sort()
-#print(names)
-print('[')
-for name in names:
-    route = name
-    file = docs[name]
-    print('  {')
-    print('    "name": "%s",' % name)
-    print('    "path": "%s",' % file)
-    print('    "route": "%s"' % route)
-    print('  },')
-print(']')
+if __name__ == '__main__':
+    res: List[Dict[str, str]] = []
+    for file in os.listdir('.'):
+        if file[-3:] == '.md':
+            path = file
+            name = file[:-3].split('-')[3:]
+            name = '-'.join(name)
+            res.append({'name': name, 'path': path, 'route': name})
+    with open('index.json', 'w') as f:
+        json.dump(res, f)
